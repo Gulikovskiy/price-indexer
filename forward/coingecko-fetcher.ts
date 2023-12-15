e7200969aac8c80e9120b1481b3f25f2853087c1
Gulikovskiy <ivan_moroz@outlook.com>
2023-12-15 19:23:55 +0200

    feat: price storing

--- forward/coingecko-fetcher.ts
+++ forward/coingecko-fetcher.ts
@@ -1,25 +1,63 @@
-"use server";
-// import "server-only";
+import "server-only";
 import { PriceRawResponse } from "../pages/api/historical-price";
-import { get } from "../session/session-store";
-export const fetchCoingeckoPrice = async () => {
-  const url = `https://api.coingecko.com/api/v3/coins/usd-coin/contract/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48/market_chart/range?vs_currency=usd&from=1701900000&to=17024839700&precision=4`;
+import { kv } from "@vercel/kv";
 
-  const res = await fetch(url);
-  if (res.status !== 200) {
-    console.error("Coingecko API failed with code %s", res.status);
-    return null;
+type Price = {
+  timestamp: number;
+  price: number;
+};
+type Response = { [symbol: string]: Price[] | null };
+
+const coinList = {
+  USDC: {
+    id: "usd-coin",
+    contract: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  },
+  WBTC: {
+    id: "wrapped-bitcoin",
+    contract: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+  },
+  UNI: {
+    id: "uniswap",
+    contract: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+  },
+};
+
+export const fetchCoingeckoPrices = async (
+  assets: string[],
+  timestamps: { start: number; finish: number }
+) => {
+  const fetchResponse: Response = {};
+  for (let i = 0; i < assets.length; i++) {
+    const symbol = assets[i];
+    const currentKey = `test-${symbol}`;
+    const stored = await kv.hgetall("test1");
+
+    const selectedAssetData = stored ? (stored[currentKey] as Price[]) : null;
+    const isDataInStorage = selectedAssetData !== undefined;
+    if (isDataInStorage) {
+      fetchResponse[symbol] = selectedAssetData;
+    } else {
+      const { id, contract } = coinList[symbol];
+      const { start, finish } = timestamps;
+      //TODO: USE `api.coingecko.com/api/v3/coins/${id}/market_chart` because of the contract address
+      const url = `https://api.coingecko.com/api/v3/coins/${id}/contract/${contract}/market_chart/range?vs_currency=usd&from=${start}&to=${finish}&precision=4`;
+      const res = await fetch(url);
+      if (res.status !== 200) {
+        console.error("Coingecko API failed with code %s", res.status);
+        return null;
+      }
+      const response = await res
+        .json()
+        .then((el: PriceRawResponse) =>
+          el.prices.map((el) => ({ timestamp: el[0], price: el[1] } as Price))
+        );
+
+      kv.hset("test1", { [currentKey]: response });
+      await (fetchResponse[symbol] = response);
+    }
   }
-  const response = res
-    .json()
-    .then((el: PriceRawResponse) =>
-      el.prices.map((el) => ({ timestamp: el[0], price: el[1] }))
-    );
 
-  // Getting the user's name from the session
-  const username = await get("username");
-
-  console.log("!!!username: ", username); // Outputs: 'John Doe'
-  return response;
-  //   res.prices.map((el) => ({ timestamp: el[0], price: el[1] }))
+  console.log("fetchResponse: ", fetchResponse);
+  return fetchResponse;
 };
--- pages/api/historical-price.ts
+++ pages/api/historical-price.ts
@@ -1,19 +1,8 @@
-// import { type NextRequest, NextResponse } from "next/server";
 import "server-only";
 import { NextRequest } from "next/server";
-import { set, get } from "../../session/session-store";
-import { fetchCoingeckoPrice } from "../../forward/coingecko-fetcher";
-
-const coinList = {
-  USDC: {
-    id: "usd-coin",
-    contract: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-  },
-  ETH: {
-    id: "ethereum",
-    contract: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
-  },
-};
+import { fetchCoingeckoPrices } from "../../forward/coingecko-fetcher";
+import { kv } from "@vercel/kv";
+// import { kv } from "@vercel/kv";
 
 export const config = {
   runtime: "edge",
@@ -29,15 +18,14 @@ function forward(route: string) {
   return async (req: NextRequest) => {
     const host = new URL(req.url).host;
     const search = new URL(req.url).search;
-    console.log("host: ", host);
-    console.log("route: ", route);
     const coins = search.substring(7, search.length).split("+");
-    console.log("coins: ", coins);
-    // Storing a user's name in the session
-    await set("username", host);
 
-    const r = await fetchCoingeckoPrice();
+    const r = await fetchCoingeckoPrices(coins, {
+      start: 1701900000,
+      finish: 17024839700,
+    });
     const test = new Response(JSON.stringify(r));
+
     return test;
   };
 }
