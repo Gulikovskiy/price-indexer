62952c59c014d37a12824f936f64f87a3bf4e4aa
Gulikovskiy <ivan_moroz@outlook.com>
2023-12-19 17:52:53 +0200

    fix: routing issues

--- pages/api/historical-price/[timestamp]/index.ts
+++ pages/api/historical-price/[timestamp]/index.ts
@@ -1,29 +1,32 @@
 import "server-only";
 import { NextRequest } from "next/server";
-import { fetchCoingeckoPrices } from "../../../../../forward/coingecko-fetcher";
-import { invalidSearchParamsError } from "../../../../../forward/utils";
-import { millisecondsInYear } from "../../../../../forward/constants";
+import { fetchCoingeckoPrices } from "../../../../forward/coingecko-fetcher";
+import { invalidSearchParamsError } from "../../../../forward/utils";
+import { millisecondsInYear } from "../../../../forward/constants";
 
 export const config = {
   runtime: "edge",
 };
 
-function forward(route: string) {
+function GET(route: string) {
   return async (req: NextRequest) => {
     const searchParams = new URL(req.url).searchParams;
 
     const timestamp = Number(searchParams.get("timestamp"));
+    console.log("timestamp:  ", timestamp);
     const timestampInMilliseconds =
       BigInt(timestamp) > BigInt(millisecondsInYear)
         ? timestamp
         : timestamp * 1000;
 
     const coins = searchParams.get("coins");
+    console.log("coins: ", coins);
 
     const properParams = timestampInMilliseconds > 0 && coins;
 
     if (properParams) {
-      const coinList = coins.split("+");
+      const coinList = coins.split(" ");
+      console.log("coinList: ", coinList);
 
       const r = await fetchCoingeckoPrices(coinList, timestampInMilliseconds);
       const resp = new Response(JSON.stringify(r));
@@ -34,7 +37,7 @@ function forward(route: string) {
   };
 }
 
-const forwarder = forward("historical-price");
+const forwarder = GET("historical-price");
 
 const handler = async (req: NextRequest) => {
   return forwarder(req);
